1e0a47194aafbd1f81b3120b8f2e19f0fd836adc
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-07-20 17:21:32 +0200

    some files created/moved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { HomepageComponent } from './features/additiveFinder/components/homepage/homepage.component';
+import { SearchpageComponent } from './features/additiveFinder/components/searchpage/searchpage.component';
+import { DetailpageComponent } from './features/additiveFinder/components/detailpage/detailpage.component';
+import { HeaderComponent } from './features/additiveFinder/components/header/header.component';
+import { FooterComponent } from './features/additiveFinder/components/footer/footer.component';
+import { MainComponent } from './features/additiveFinder/container/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SearchpageComponent,
+    DetailpageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
