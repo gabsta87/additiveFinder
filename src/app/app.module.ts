c4fa24dbbabc96c4f8f191a5b8999feb40a58fb7
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-07-21 09:30:06 +0200

    init works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
-import { HomepageComponent } from './features/additiveFinder/components/homepage/homepage.component';
-import { SearchpageComponent } from './features/additiveFinder/components/searchpage/searchpage.component';
-import { DetailpageComponent } from './features/additiveFinder/components/detailpage/detailpage.component';
-import { HeaderComponent } from './features/additiveFinder/components/header/header.component';
-import { FooterComponent } from './features/additiveFinder/components/footer/footer.component';
-import { MainComponent } from './features/additiveFinder/container/main/main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    SearchpageComponent,
-    DetailpageComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([{
+      path:'home',
+      loadChildren:()=>import("./features/additive-finder/additive-finder.module").then(fichier=>fichier.AdditiveFinderModule)
+    },{
+      path:'',
+      redirectTo:'home',
+      pathMatch:'full'
+    },{
+      path:'**',
+      redirectTo:'home',
+      pathMatch:'full'
+    }]),
     IonicModule.forRoot()
   ],
   providers: [],
