a9cdc81db3c850972fd2a3fdc38bb61838315163
gabsta87 <gabrielmaretsecondaire@gmail.com>
2022-07-21 10:18:30 +0200

    files added

---
+++ src/app/features/additive-finder/pipes/danger-color.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DangerColorPipe } from './danger-color.pipe';
+
+describe('DangerColorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DangerColorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/features/additive-finder/pipes/danger-color.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { OutletContext } from '@angular/router';
+
+@Pipe({
+  name: 'dangerColor'
+})
+export class DangerColorPipe implements PipeTransform {
+
+  transform(value: number): string {
+    let output;
+    switch(value){
+      case 0:
+        output = "green";
+        break;
+      case 1:
+        output = "yellow";
+        break;
+      case 2:
+        output = "orange";
+        break;
+      case 3:
+        output = "red";
+        break;
+      default:
+        output = "lightgrey";
+        break;
+    };
+    return output;
+  }
+}
